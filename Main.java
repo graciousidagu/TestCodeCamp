6d35efff94998025914e486e4e3f1751ba22cab3
graciousidagu <idagugracious4@gmail.com>
2022-03-28 23:36:37 +0100

    added java task

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello world.This is my first project.");
+    System.out.println("My name is gracious.This is my first project.");
+    System.out.println("I am a student in testify.This is my first project.");
+    System.out.println("the course am taking on software testing will be ending soon.This is my first project");
+    System.out.println("I have a wonderful team.This is my first project");
+    System.out.println("I am not feeling well.This is my first project");
+    System.out.println("I am in team githup.This is my first project");
+    }
+}
